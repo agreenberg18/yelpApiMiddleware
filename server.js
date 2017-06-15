a1c76f0074161541d88d49ac5c812c9455d20bdf
Andrew Greenberg <andrewgreenberg@Andrews-MacBook-Air.local>
2017-06-14 22:34:03 -0400

    working nodejs to yelp api

---
+++ server.js
@@ -0,0 +1,52 @@
+var express = require('express'); 
+var request = require('request'); 
+var app = express();
+
+var myParser = require("body-parser");
+//support parsing of application/json type post data
+app.use(myParser.json());
+
+//support parsing of application/x-www-form-urlencoded post data
+app.use(myParser.urlencoded({ extended: true }));
+
+
+var options = {
+ // url: 'http://api.yelp.com/v3/businesses/search?categories=restaurants&latitude=37.786882&longitude=-122.399972',
+  headers: {
+    'Authorization': 'bearer RT2S0q88HosnKEJrds-Lbe5EhebLejgURB7JzQeK7lC_qNI_PCQL4XuR5yE7YfIcdX_GfDf9H7kwI44cyRf855v71v67Gpkue-ZjgoQTBJyDtL_7YJKxHPUsRkk4WXYx'
+  }
+};
+
+// app.use(myParser.urlencoded({extended : true}));
+//    app.post("/getRestaurants", function(request, response) {
+//        console.log(request.body);
+//  });
+
+
+// app.get('/api', function(req, res){ 
+//   request(options, function (error, response, body) { 
+//     if (!error && response.statusCode === 200) { 
+//       console.log(body); 
+//       res.send(body); 
+//     } 
+//    }); 
+// });
+
+// app.get('/api/:location', function(req, res) {
+//   res.send("long is set to " + req.params.location);
+// });
+
+app.get('/api/:location', function(req, res){
+  console.log(req.params.longitude) 
+  request('https://api.yelp.com/v3/businesses/search?categories=restaurants&' + req.params.location, options, 
+  function (error, response, body) { 
+    if (!error && response.statusCode === 200) { 
+      console.log(body); 
+      res.send(body); 
+    } 
+   }); 
+});
+
+
+app.listen(3000); 
+console.log('Server running on port %d', 3000);
