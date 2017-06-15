e28c8772d78fa872e25dfba7ef11cb93c59850da
Andrew Greenberg <andrewgreenberg@Andrews-MacBook-Air.local>
2017-06-15 17:05:27 -0400

    what is life

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express'); 
 var request = require('request'); 
+var cors = require('cors')
 var app = express();
+
+app.use(cors())
 
 var myParser = require("body-parser");
 //support parsing of application/json type post data
@@ -32,20 +35,20 @@ var options = {
 //    }); 
 // });
 
-app.get('/', function(req, res) {
-  res.send("long is set to " + 'req.params.location');
-});
-
-// app.get('/:location', function(req, res){
-//   console.log(req.params.location) 
-//   request('https://api.yelp.com/v3/businesses/search?categories=restaurants&' + req.params.location, options, 
-//   function (error, response, body) { 
-//     if (!error && response.statusCode === 200) { 
-//       console.log(body); 
-//       res.send(body); 
-//     } 
-//    }); 
+// app.get('/', function(req, res) {
+//   res.send("long is set to " + 'req.params.location');
 // });
+
+app.get('/:location', function(req, res){
+  console.log(req.params.location) 
+  request('https://api.yelp.com/v3/businesses/search?categories=restaurants&' + req.params.location, options, 
+  function (error, response, body) { 
+    if (!error && response.statusCode === 200) { 
+      console.log(body); 
+      res.send(body); 
+    } 
+   }); 
+});
 
 
 //app.listen(3000);
